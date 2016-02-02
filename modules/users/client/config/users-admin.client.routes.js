787ea6fbe9bc9da0e3d3fc27d1898157db3b09e3
Francis Daigle <francis@francisdaigle.com>
2016-02-01 23:13:03 -0700

    fix(users): Remove unused routes
    Fixes #10

--- modules/users/client/config/users-admin.client.routes.js
+++ modules/users/client/config/users-admin.client.routes.js
@@ -8,29 +8,5 @@ angular.module('users.admin.routes').config(['$stateProvider',
         url: '/users',
         templateUrl: 'modules/users/client/views/admin/users.client.view.html'
       });
-      // .state('admin.user', {
-      //   url: '/users/:userId',
-      //   templateUrl: 'modules/users/client/views/admin/view-user.client.view.html',
-      //   controller: 'UserController',
-      //   resolve: {
-      //     userResolve: ['$stateParams', 'Admin', function ($stateParams, Admin) {
-      //   //     return Admin.get({
-      //   //       userId: $stateParams.userId
-      //   //     });
-      //     }]
-      //   }
-      // })
-      // .state('admin.user-edit', {
-      //   url: '/users/:userId/edit',
-      //   templateUrl: 'modules/users/client/views/admin/edit-user.client.view.html',
-      //   controller: 'UserController',
-      //   resolve: {
-      //     userResolve: ['$stateParams', 'Admin', function ($stateParams, Admin) {
-      //   //     return Admin.get({
-      //   //       userId: $stateParams.userId
-      //   //     });
-      //     }]
-      //   }
-      // });
   }
 ]);
